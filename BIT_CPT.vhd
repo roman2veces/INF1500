cba17c7049bb215d6890170040f99edd09ba3f89
roman2veces <68816785+roman2veces@users.noreply.github.com>
2022-03-14 21:43:49 -0400

    Update BIT_CPT.vhd

--- BIT_CPT.vhd
+++ BIT_CPT.vhd
@@ -20,7 +20,7 @@
 
 
 library IEEE;
-use IEEE.STD_LOGIC_1164.ALL;
+-- use IEEE.STD_LOGIC_1164.ALL;
 use IEEE.NUMERIC_STD.ALL;
 
 -- Uncomment the following library declaration if using
@@ -41,49 +41,21 @@ entity BIT_CPT is
 end BIT_CPT;
 
 architecture FlotDeDonnees of BIT_CPT is
+
+
+component NO_ZEROS
+    port(A: in unsigned(3 downto 0); no_zeros: out integer);
+end component;
+
 signal no_zeros_A: integer; 
 signal no_zeros_B: integer; 
-begin    
-    with A select 
-    no_zeros_A <=
-    4 when "0000",
-    3 when "0001",
-    3 when "0010",
-    2 when "0011",
-    3 when "0100",
-    2 when "0101",
-    2 when "0110",
-    1 when "0111",
-    3 when "1000",
-    2 when "1001",
-    2 when "1010",
-    1 when "1011",
-    2 when "1100",
-    1 when "1101",
-    1 when "1110",
-    0 when "1111",
-    0 when others; -- There are not really others
 
-    with B select 
-    no_zeros_B <=
-    4 when "0000",
-    3 when "0001",
-    3 when "0010",
-    2 when "0011",
-    3 when "0100",
-    2 when "0101",
-    2 when "0110",
-    1 when "0111",
-    3 when "1000",
-    2 when "1001",
-    2 when "1010",
-    1 when "1011",
-    2 when "1100",
-    1 when "1101",
-    1 when "1110",
-    0 when "1111",
-    0 when others; -- There are not really others
+begin    
+    U1: NO_ZEROS port map(A => A, no_zeros => no_zeros_A);
+    U2: NO_ZEROS port map(A => B, no_zeros => no_zeros_B);
+     
     -- Maybe convert to std_logic_vector
    S <= to_unsigned(no_zeros_A + no_zeros_B, S'length);
     
 end FlotDeDonnees;
+
