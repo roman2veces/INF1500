63fdd1933355e8fdc59c39255fdabad5948489c7
roman2veces <68816785+roman2veces@users.noreply.github.com>
2022-03-21 19:18:06 -0400

    Update SEVEN_SEG.vhd

--- lab_4/SEVEN_SEG.vhd
+++ lab_4/SEVEN_SEG.vhd
@@ -1,3 +1,24 @@
+----------------------------------------------------------------------------------
+-- Company: 
+-- Engineer: 
+-- 
+-- Create Date: 03/21/2022 06:36:04 PM
+-- Design Name: 
+-- Module Name: SEVEN_SEG - FlotDeDonnees
+-- Project Name: 
+-- Target Devices: 
+-- Tool Versions: 
+-- Description: 
+-- 
+-- Dependencies: 
+-- 
+-- Revision:
+-- Revision 0.01 - File Created
+-- Additional Comments:
+-- 
+----------------------------------------------------------------------------------
+
+
 library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
 use IEEE.NUMERIC_STD.ALL;
@@ -7,7 +28,7 @@ entity SEVEN_SEG is
         premier_nombre: in UNSIGNED(3 downto 0);
         deuxieme_nombre: in UNSIGNED(3 downto 0);
         SEL: in UNSIGNED(2 downto 0);
-        S: out UNSIGNED(7 downto 0);
+        SEVEN_SEG: out UNSIGNED(7 downto 0);
         AN: out UNSIGNED(7 downto 0)
     );
 end SEVEN_SEG;
@@ -41,22 +62,22 @@ end component BIT_PAR;
 
 component MUX_5
     Port ( 
-        A: in std_logic_vector(3 downto 0);
-        B: in std_logic_vector(3 downto 0);
-        ADD: in std_logic_vector(4 downto 0);
-        BIT_CPT: in std_logic_vector(4 downto 0);
-        BIT_PAR: in std_logic_vector(4 downto 0);
-        SEL: in std_logic_vector(2 downto 0);
-        S: out std_logic_vector(4 downto 0)
+        A: in unsigned(3 downto 0);
+        B: in unsigned(3 downto 0);
+        ADD: in unsigned(4 downto 0);
+        BIT_CPT: in unsigned(4 downto 0);
+        BIT_PAR: in unsigned(4 downto 0);
+        SEL: in unsigned(2 downto 0);
+        S: out unsigned(4 downto 0)
     );
 end component MUX_5;
 
-component 7SEG_CONV
+component SEVEN_SEG_CONV
     Port ( 
         A: in unsigned(4 downto 0);
         S: out unsigned(7 downto 0)
     );
-end component 7SEG_CONV;
+end component SEVEN_SEG_CONV;
 
 -- SIGNALS
 signal add_resultat: unsigned(4 downto 0);
@@ -69,7 +90,7 @@ begin
     U1: ADD port map(A => premier_nombre, B => deuxieme_nombre, S => add_resultat);
     U2: BIT_CPT port map(A => premier_nombre, B => deuxieme_nombre, S => bit_cpt_resultat);
     U3: BIT_PAR port map(A => premier_nombre, B => deuxieme_nombre, S => bit_par_resultat);
-    U3: MUX_5 port map(
+    U4: MUX_5 port map(
         A => premier_nombre, 
         B => deuxieme_nombre, 
         ADD => add_resultat,
@@ -78,7 +99,7 @@ begin
         SEL => SEL,
         S => caractere_7seg
     );
-    U3: 7SEG_CONV port map(A => caractere_7seg, S => S);
+    U5: SEVEN_SEG_CONV port map(A => caractere_7seg, S => SEVEN_SEG);
     AN <= "11111110";
 
 end FlotDeDonnees;
