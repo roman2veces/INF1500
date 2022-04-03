8a15bd55845dac9aa5d8a8edde9509ba279506cf
roman2veces <68816785+roman2veces@users.noreply.github.com>
2022-04-02 23:48:14 -0400

    Update test_2_digilock.vhd

--- lab_5/test_2_digilock.vhd
+++ lab_5/test_2_digilock.vhd
@@ -51,12 +51,23 @@ end DigiLock;
 
 architecture Behavioral of DigiLock is
 
-    type Etat is (Init, E0, E1, E2, E3, Ouvert, Alarme, Alarm_0, Alarm_1);
+    type Etat is (Init, waiting, E0, E1, E2, Ouvert, Alarme, Alarm_0);
     --type Alarm_state is (Alarm_init, alarm_i0, alarm_i1);
     signal etat_present: Etat := Init;
     signal etat_prochain: Etat := Init;
-   -- signal current_alarm_state: Alarm_state := Alarm_init;
+    signal unique_A :std_logic;
+    signal unique_B :std_logic;
+    signal unique_C :std_logic;
     
+    component pulse_generator is 
+        Port ( 
+            clk: in std_logic;
+            reset: in std_logic;
+            input: in std_logic;
+            output: out std_logic
+        );
+    end component;
+        
   --  COMPONENT DEBOUNCE IS
   --    GENERIC(
   --      counter_size  :  INTEGER := 19); --counter size (19 bits gives 10.5ms with 50MHz clock)
@@ -69,137 +80,124 @@ architecture Behavioral of DigiLock is
     signal debounced_A: std_logic;
     signal debounced_B: std_logic;
     signal debounced_C: std_logic;
-    signal pressed_value: std_logic_vector(3 downto 0) := "UUUU";
-    signal in_alarme: std_logic := '0';
-   -- signal resultat_B: std_logic_vector(3 downto 0);
-   -- signal resultat_C: std_logic_vector(3 downto 0);
-    
+    signal pressed_value: std_logic_vector(3 downto 0) := "0000";   
 
 begin
     --U1: DEBOUNCE port map(clk => clk, button => A, result => debounced_A);
     --U2: DEBOUNCE port map(clk => clk, button => B, result => debounced_B);
     --U3: DEBOUNCE port map(clk => clk, button => C, result => debounced_C);
-
-    process (clk, reset, A, B, C)
-    begin
-        -- TODO: changer pour debounced_A
-        if A = '1' then
-            pressed_value <= "0001";
-        elsif B = '1' then
-            pressed_value <= "0010";
-        elsif C = '1' then 
-            pressed_value <= "0011";
-        else 
-            pressed_value <= "UUUU";
-        end if; 
-        
-        if reset = '1' then
+    U1: pulse_generator port map(clk => clk, reset => reset,  input => A, output => unique_A);
+    U2: pulse_generator port map(clk => clk, reset => reset,  input => B, output => unique_B);
+    U3: pulse_generator port map(clk => clk, reset => reset,  input => C, output => unique_C);
+    
+    process (clk, reset)
+    begin        
+        if reset = '0' then
             etat_present <= Init;
-        elsif rising_edge(clk) and pressed_value /= "UUUU" then
+        elsif rising_edge(clk) then
             etat_present <= etat_prochain;
         end if;
     end process;
     
-    --process (debounced_A, debounced_B, debounced_C) 
-    --process (A, B, C) 
-    --begin
-      --  if A = '1' then
-        --    pressed_value <= "0001";
-        --elsif B = '1' then
-          --  pressed_value <= "0010";
-       -- elsif C = '1' then 
-        --    pressed_value <= "0011";
-        --else 
-          --  pressed_value <= "UUUU";
-        --end if; 
-    --end process;
+    process (unique_A, unique_B, unique_C)
+    begin 
+        if unique_A = '1' then
+            pressed_value <= "0001";
+        elsif unique_B = '1' then
+            pressed_value <= "0010";
+        elsif unique_C = '1' then 
+            pressed_value <= "0011";
+        else 
+            pressed_value <= "0000";
+        end if;
+    end process;
     
-    -- TODO: ajouter peutetre debounceds 
-    process (etat_present, pressed_value) 
+    process (pressed_value) 
         variable entree_0: std_logic_vector(3 downto 0);
         variable entree_1: std_logic_vector(3 downto 0);
         variable entree_2: std_logic_vector(3 downto 0);
         variable entree_3: std_logic_vector(3 downto 0);
-        variable entree_4: std_logic_vector(3 downto 0);
-        variable entree_5: std_logic_vector(3 downto 0);
         variable compteur: integer RANGE 0 TO 4 := 0;
-    begin
-
+    begin 
         
-            case etat_present is 
-                when Init =>
+        case etat_present is 
+            when Init => 
+                SEG0 <= "0100"; -- 0100 = 4 = L
+                SEG1 <= "0000"; -- 0000 = 0 = -
+                SEG2 <= "0000";
+                SEG3 <= "0000"; 
+                etat_prochain <= waiting; 
+            
+            when waiting =>        
+                if pressed_value /= "0000" then
+                    entree_0 := pressed_value;
+                    SEG0 <= pressed_value;
+                    etat_prochain <= E0;
+                end if;
+          
+            when E0 => 
+                if pressed_value /= "0000" then    
+                    entree_1 := pressed_value;
+                    SEG1 <= pressed_value;
+                    etat_prochain <= E1;
+                end if;
+                
+            when E1 =>
+                if pressed_value /= "0000" then
+                    entree_2 := pressed_value;
+                    SEG2 <= pressed_value;
+                    etat_prochain <= E2;
+                end if;
+                
+            when E2 =>
+                if pressed_value /= "0000" then
+                    entree_3 := pressed_value;
+                    SEG3 <= pressed_value;
+                    
+                    if entree_0 = "0011" and entree_1 = "0001" and entree_2 = "0011" and entree_3 = "0010" then -- CODE = 0011000100110010 = CACB
+                        SEG0 <= "0101"; -- 0101 = 5 = O
+                        SEG1 <= "0101";
+                        SEG2 <= "0101";
+                        SEG3 <= "0101";
+                        etat_prochain <= Ouvert;
+                    elsif compteur = 2 then  -- car il commence a zero
+                        SEG0 <= "0000";
+                        SEG1 <= "0000";
+                        SEG2 <= "0100"; -- 0100 = 4 = L
+                        SEG3 <= "0001"; -- 0001 = 1 = A 
+                        compteur := 0;
+                        etat_prochain <= Alarme;
+                    else 
+                        compteur := compteur + 1;
+                        SEG0 <= "0100"; -- 0100 = 4 = L
+                        SEG1 <= "0000"; -- 0000 = 0 = -
+                        SEG2 <= "0000";
+                        SEG3 <= "0000";
+                        etat_prochain <= waiting;
+                    end if;                       
+                end if;
+                
+            when Ouvert =>
+                if unique_A = '1' or unique_B = '1' or unique_C = '1' then
+                    etat_prochain <= waiting;
+                end if;
+                            
+            when Alarme => 
+                if pressed_value = "0011" then  
+                    etat_prochain <= Alarm_0;
+                end if;
+                           
+            when Alarm_0 => 
+                if pressed_value = "0010" then
                     SEG0 <= "0100"; -- 0100 = 4 = L
                     SEG1 <= "0000"; -- 0000 = 0 = -
                     SEG2 <= "0000";
                     SEG3 <= "0000";
-                    if pressed_value /= "UUUU" then
-                        entree_0 := pressed_value;
-                        SEG0 <= pressed_value;
-                        etat_prochain <= E0;
-                  --  else 
-                  --      etat_prochain <= Init;
-                    end if;
-                
-                when E0 =>     
-                   entree_1 := pressed_value;
-                   SEG1 <= pressed_value;
-                   etat_prochain <= E1;
-                
-                when E1 =>
-                    entree_2 := pressed_value;
-                    SEG2 <= pressed_value;
-                    etat_prochain <= E2;
-                
-                when E2 =>
-                   entree_3 := pressed_value;
-                   SEG3 <= pressed_value;
-                   etat_prochain <= E3;
-                   
-                when E3 => 
-                    if entree_0 = "0011" and entree_1 = "0001" and entree_2 = "0011" and entree_3 = "0010" then  -- CODE = 0011000100110010 = CACB
-                        etat_prochain <= Ouvert;
-                    elsif compteur = 3 then
-                        etat_prochain <= Alarme;
-                    else 
-                        compteur := compteur + 1;
-                        etat_prochain <= Init;
-                    end if; 
-                    
-                when Ouvert =>
-                    SEG0 <= "0101"; -- 0101 = 5 = O
-                    SEG1 <= "0101";
-                    SEG2 <= "0101";
-                    SEG3 <= "0101";
-                    --if debounced_A = '1' or debounced_B = '1' or debounced_C = '1' then 
-                    if A = '1' or B = '1' or C = '1' then
-                        etat_prochain <= Init;
-                    end if;
-                
-                when Alarme =>    
-                    SEG0 <= "0000"; -- 0101 = 5 = O
-                    SEG1 <= "0000";
-                    SEG2 <= "0100"; -- 0100 = 4 = L
-                    SEG3 <= "0001"; -- 0001 = 1 = A
-                    etat_prochain <= Alarm_0;
-                  --in_alarme <= '1';
-               
-                when Alarm_0 => 
-                    entree_4 := pressed_value;
-                    SEG3 <= pressed_value;
-                    etat_prochain <= Alarm_1;
-                    
-                when Alarm_1 =>
-                    entree_5 := pressed_value;   
-                    SEG2 <= pressed_value;
-                    if entree_4 = "0011" and entree_5 = "0010" then   
-                        etat_prochain <= Init;
-                    else
-                        etat_prochain <= Alarme; 
-                    end if;
-                    
-                 
-             --   when others =>
-                 --   etat_prochain <= Init;
+                    etat_prochain <= waiting;
+                elsif pressed_value = "0001" then
+                    etat_prochain <= Alarme;
+                end if;
+                                
             end case;      
     end process;    
-end Behavioral;     
+end Behavioral;
